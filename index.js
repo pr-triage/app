509a0930428445f3ab55904deeb635b53b97a591
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2017-11-20 01:27:56 +0900

    chore(log): Add debug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const debug = require('debug')('probot:pr-label')
 const handlePullRequestChange = require('./lib/handle-pull-request-change')
 
 function probotPlugin (robot) {
+  debug('ready to receive GitHub webhooks')
   robot.on([
     'pull_request.opened',
     'pull_request.edited',
